dbd43e8ba9edc40f55da18e4f1269a87fba02c2f
Anida Assane Yacouba <aniyacoub18@gmail.com>
2024-04-04 22:26:23 +0000

    initialisation de l'application

---
+++ exodevnet.py
@@ -0,0 +1,15 @@
+import requests
+
+def recherche_adresse():
+    try:
+        print("########Résultats####### \n")
+       
+        response = requests.get('https://ipapi.co/json/')
+        response_json = response.json()
+        print(f"Adresse : {response_json['ip']}\nversion ip: {response_json['version']}\nPays: {response_json['country']}\nVille: {response_json['city']} ")
+    except Exception as e:
+        print(f"Probleme survenu")
+    finally:
+        print("\n########Dieuredieuf#######")
+
+recherche_adresse()
